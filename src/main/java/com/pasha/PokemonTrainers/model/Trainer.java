49ef21a0c6bcf947b5d2850fbb2ffdd617521152
prawira <robbyprawira3@gmail.com>
2023-11-16 15:02:14 +0700

    Create Trainer Model

---
+++ src/main/java/com/pasha/PokemonTrainers/model/Trainer.java
@@ -0,0 +1,58 @@
+package com.pasha.PokemonTrainers.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "trainers")
+public class Trainer {
+
+    @Id
+    @SequenceGenerator(
+        name = "trainer_id_sequence",
+        sequenceName = "trainer_id_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "trainer_id_sequence"
+    )
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "trainer", fetch = FetchType.LAZY)
+    private List<Pokemon> pokemons;
+}

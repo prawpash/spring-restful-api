3262b89a68009b4b1a6d7057791013124faa4e4d
prawira <robbyprawira3@gmail.com>
2023-11-16 18:41:34 +0700

    Update Trainer Repository
    Menambahkan fungsi untuk mencari data trainer berdasarkan username

--- src/main/java/com/pasha/PokemonTrainers/repository/TrainerRepository.java
+++ src/main/java/com/pasha/PokemonTrainers/repository/TrainerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pasha.PokemonTrainers.model.Trainer;
 
 public interface TrainerRepository extends JpaRepository<Trainer, Integer>{
-    
+    Trainer findByUsername(String username);
 }

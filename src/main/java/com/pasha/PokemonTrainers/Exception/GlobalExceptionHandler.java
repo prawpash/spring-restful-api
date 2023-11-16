c5e2e92b384313f5a8e2b53dee2d6bd990001fc8
prawira <robbyprawira3@gmail.com>
2023-11-16 23:27:56 +0700

    Update Exception Handler
    Membuat format error menjadi seperti format response yang telah
    ditentukan

--- src/main/java/com/pasha/PokemonTrainers/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/pasha/PokemonTrainers/Exception/GlobalExceptionHandler.java
@@ -3,15 +3,18 @@ package com.pasha.PokemonTrainers.Exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.pasha.PokemonTrainers.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
+    public ResponseEntity<ResponseDto<Map<String, String>>> handleValidationExceptions(
         MethodArgumentNotValidException ex
     ) {
           Map<String, String> errors = new HashMap<>();
@@ -20,6 +23,19 @@ public class GlobalExceptionHandler {
               String errorMessage = error.getDefaultMessage();
               errors.put(fieldName, errorMessage);
           });
-          return errors;
+          return ResponseEntity
+                  .status(ex.getStatusCode())
+                  .body(
+                          ResponseDto
+                                  .<Map<String, String>>builder()
+                                  .errors(errors)
+                                  .build()
+                  );
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ResponseDto<String>> apiException(ResponseStatusException ex){
+        return ResponseEntity
+                .status(ex.getStatusCode()).body(ResponseDto.<String>builder().errors(ex.getReason()).build());
     }
 }

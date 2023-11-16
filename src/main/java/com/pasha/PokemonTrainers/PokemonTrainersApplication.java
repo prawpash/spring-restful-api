1c9b50d4d41ec6e074b08e8bee6344a3e81feb8e
prawira <robbyprawira3@gmail.com>
2023-11-16 14:29:20 +0700

    🎉 Initial Commit

---
+++ src/main/java/com/pasha/PokemonTrainers/PokemonTrainersApplication.java
@@ -0,0 +1,13 @@
+package com.pasha.PokemonTrainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTrainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonTrainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasha/PokemonTrainers/PokemonTrainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasha.PokemonTrainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTrainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

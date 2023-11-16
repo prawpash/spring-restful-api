e9bb69265058a3f30fd8c42f33e514ffb4978a50
prawira <robbyprawira3@gmail.com>
2023-11-16 18:44:07 +0700

    Update Pokemon Dto
    Menambahkan beberapa fungsi untuk memastikan bahwa data name ,ability
    dan trainer_id tidak kosong

--- src/main/java/com/pasha/PokemonTrainers/dto/PokemonDto.java
+++ src/main/java/com/pasha/PokemonTrainers/dto/PokemonDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import com.pasha.PokemonTrainers.model.Trainer;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +17,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PokemonDto {
     private Integer id;
+    @NotBlank(message = "Pokemon Name should not be empty")
     private String name;
+    @NotBlank(message = "Pokemon Ability should not be empty")
     private String ability;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    @Min(value = 1, message = "Pokemon Trainer should not be empty")
+    private Integer trainer_id;
     private Trainer trainer;
 }

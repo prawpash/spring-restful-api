93dd8141bfab3315fdd9023146937cc24cd71464
prawira <robbyprawira3@gmail.com>
2023-11-16 18:43:03 +0700

    Update Trainer Dto
    Menambahkan beberapa fungsi untuk mengecek supaya data name, username
    dan password tidak kosong

--- src/main/java/com/pasha/PokemonTrainers/dto/TrainerDto.java
+++ src/main/java/com/pasha/PokemonTrainers/dto/TrainerDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.pasha.PokemonTrainers.model.Pokemon;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,8 +17,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TrainerDto {
     private Integer id;
+    @NotBlank(message = "Trainer name should not be empty")
     private String name;
+    @NotBlank(message = "Trainer username should not be empty")
     private String username;
+    @NotBlank(message = "Trainer password should not be empty")
     private String password;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;

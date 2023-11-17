7fad7214129dd50b7f52d213b2d1bf7e5304d051
prawira <robbyprawira3@gmail.com>
2023-11-17 13:04:41 +0700

    Update Pokemon Repository
    Menambahkan fungsi untuk mengambil data pokemon berdasarkan id trainer

--- src/main/java/com/pasha/PokemonTrainers/repository/PokemonRepository.java
+++ src/main/java/com/pasha/PokemonTrainers/repository/PokemonRepository.java
@@ -3,7 +3,12 @@ package com.pasha.PokemonTrainers.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pasha.PokemonTrainers.model.Pokemon;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer>{
-    
+    @Query(value = "SELECT * FROM pokemons p WHERE p.trainer_id = :id", nativeQuery = true)
+    List<Pokemon> findByTrainerId(@Param("id") Integer id);
 }

cbb3b4752b1237f26fff0054e1533f50889385dc
prawira <robbyprawira3@gmail.com>
2023-11-17 09:07:00 +0700

    Update Triner Repository
    Menambahkan fungsi untuk mencari Trainer berdasarkan username dan tidak
    memiliki id yang sama dengan yang diinputkan

--- src/main/java/com/pasha/PokemonTrainers/repository/TrainerRepository.java
+++ src/main/java/com/pasha/PokemonTrainers/repository/TrainerRepository.java
@@ -3,7 +3,14 @@ package com.pasha.PokemonTrainers.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pasha.PokemonTrainers.model.Trainer;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TrainerRepository extends JpaRepository<Trainer, Integer>{
     Trainer findByUsername(String username);
+
+    @Query(value = "SELECT * FROM trainers t WHERE t.username = :username AND t.id != :id", nativeQuery = true)
+    Optional<Trainer> findByUsernameExceptId(@Param("username") String username,@Param("id") Integer id);
 }
